97d4d6b81cc6b7d365b593338d47a4eb714eafa8
Subrat <skbehera@live.in>
2024-09-18 23:18:39 +0530

    Added Json schema validation

--- src/main/java/com/example/jsonschema/JsonValidator.java
+++ src/main/java/com/example/jsonschema/JsonValidator.java
@@ -1,39 +1,41 @@
 package com.example.jsonschema;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.networknt.schema.JsonSchema;
 import com.networknt.schema.JsonSchemaFactory;
 import com.networknt.schema.SpecVersion;
 import com.networknt.schema.ValidationMessage;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
 
 public class JsonValidator {
 
-    private static InputStream inputStreamFromClasspath(String path) {
-        return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
-    }
+    public Set<ValidationMessage> validateJsonSchema(String schemaFilePath, String jsonStringPath) throws IOException {
 
-    public static void main(String[] args) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
+        final var objectMapper = new ObjectMapper();
+        final var schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
 
-        try (InputStream jsonStream = inputStreamFromClasspath("input.json");
-             InputStream schemaStream = inputStreamFromClasspath("schema.json")) {
+        try (final var jsonStream = inputStreamFromClasspath("json/validInput1.json");
+             final var schemaStream = inputStreamFromClasspath("json/schema.json")) {
 
-            JsonNode json = objectMapper.readTree(jsonStream);
-            JsonSchema schema = schemaFactory.getSchema(schemaStream);
+            final var json = objectMapper.readTree(jsonStream);
+            final var  schema = schemaFactory.getSchema(schemaStream);
 
-            Set<ValidationMessage> validationResult = schema.validate(json);
+            final var validationResult = schema.validate(json);
 
             if (validationResult.isEmpty()) {
                 System.out.println("No validation errors :-)");
             } else {
                 validationResult.forEach(vm -> System.out.println(vm.getMessage()));
             }
+
+            return validationResult;
         }
+    }
+
+    private static InputStream inputStreamFromClasspath(String path) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
     }
 }
 
--- src/test/java/com/example/jsonschema/JsonValidatorTest.java
+++ src/test/java/com/example/jsonschema/JsonValidatorTest.java
@@ -1,4 +1,49 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example.jsonschema;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+
 class JsonValidatorTest {
-  
+
+    private JsonValidator jsonValidator = null;
+
+    @BeforeEach
+    void setUp() {
+        jsonValidator = new JsonValidator();
+    }
+
+    @Test
+    void validateJsonSchema_valid() throws IOException {
+        final var validationResultSet = jsonValidator.validateJsonSchema(
+                "json/validInput1.json", "json/schema.json");
+
+
+    }
+
+    @Test
+    void validateJsonSchema_invalid1() throws IOException {
+        final var validationResultSet = jsonValidator.validateJsonSchema(
+                "json/invalidInput1.json", "json/schema.json");
+
+
+    }
+
+    @Test
+    void validateJsonSchema_invalid2() throws IOException {
+        final var validationResultSet = jsonValidator.validateJsonSchema(
+                "json/invalidInput2.json", "json/schema.json");
+
+
+    }
+
+    @Test
+    void validateJsonSchema_invalid3() throws IOException {
+        final var validationResultSet = jsonValidator.validateJsonSchema(
+                "json/invalidInput3.json", "json/schema.json");
+
+
+    }
 }
